2ea3cd067df5bfd10765868d98781613e4765fbd
haithemhammami <148556823+haithemhammami@users.noreply.github.com>
2024-03-29 17:34:55 +0100

    Update serveur.js

--- testprojet.js/src/serveur.js
+++ testprojet.js/src/serveur.js
@@ -1,65 +1,3 @@
-/*const express = require('express');
-const path = require('path');
-const axios = require('axios');
-
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-;
-// Ajouter cette ligne pour gérer les requêtes GET vers pokemon-details.html
-app.get('/pokemon-details.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'pokemon-details.html'));
-});
-
-
-// Route pour récupérer la liste des Pokémon
-app.get('/pokemon', async (req, res) => {
-  try {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-    const pokemonList = response.data.results;
-    res.json(pokemonList);
-  } catch (error) {
-    console.error('Erreur lors de la récupération de la liste des Pokémon:', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération de la liste des Pokémon' });
-  }
-});
-
-
-// Route pour récupérer les détails d'un Pokémon spécifique
-app.get('/pokemon/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemonDetails = response.data;
-    res.json(pokemonDetails);
-  } catch (error) {
-    console.error(`Erreur lors de la récupération des détails du Pokémon avec l'ID ${id}:`, error);
-    res.status(500).json({ error: `Erreur lors de la récupération des détails du Pokémon avec l'ID ${id}` });
-  }
-});
-
-
-// Route pour la racine de l'application
-//app.get('/', (req, res) => {
- // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//});
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`Le serveur écoute sur le port ${PORT}`);
-});
-*/
-
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
@@ -69,11 +7,11 @@ const { Resend } = require('resend');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Utiliser bodyParser pour analyser les données du formulaire
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Initialisez Resend avec le clé d'API
-const resend = new Resend('re_Q7hpdpfH_8waY9mcxP8PoZj24BYwUoevD');
+// Initialisez Resend avec le clé d'API 
+const resend = new Resend('votre clé');
 
 
 // Servir les fichiers statiques
@@ -101,7 +39,7 @@ app.get('/pokemon', async (req, res) => {
   }
 });
 
-// Route pour récupérer les détails d'un Pokémon spécifique
+// Route pour récupérer les détails d'un Pokémon 
 app.get('/pokemon/:id', async (req, res) => {
   const { id } = req.params;
   try {
@@ -114,14 +52,14 @@ app.get('/pokemon/:id', async (req, res) => {
   }
 });
 
-// Route pour gérer l'envoi d'e-mail depuis le formulaire de contact
+// Route pour gérer l'envoi d'e-mail 
 app.post('/send-email', (req, res) => {
     const { Nom, Prénom, date_de_naissance, Email } = req.body;
 
-    // Envoyer l'e-mail
+  
     resend.emails.send({
         from: 'onboarding@resend.dev',
-        to: 'haithem192000@gmail.com',
+        to: 'votre email',
         subject: 'Sujet de l\'e-mail',
         html: `<p>Bonjour ${Prénom} ${Nom},</p>
                <p>Merci pour votre message. Votre date de naissance est ${date_de_naissance}.</p>`
@@ -135,7 +73,7 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-// Écouter sur le port spécifié
+
 app.listen(PORT, () => {
   console.log(`Le serveur écoute sur le port ${PORT}`);
 });
